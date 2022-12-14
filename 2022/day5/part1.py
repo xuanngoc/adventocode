027efa58d8450a17d3c005621f09827dd29e472e
xuanngoc <ngocbui.se@gmail.com>
2022-12-15 02:50:09 +0700

    done day5 part 1

--- 2022/day5/part1.py
+++ 2022/day5/part1.py
@@ -33,6 +33,7 @@ m = np.array(crates)
 crates = np.rot90(m, 3).tolist()
 
 for i in range(0, len(crates)):
+  print(i+1, end=' ')
   for m in range(0, len(crates[0])):
     print(crates[i][m], end=' ')
   print('')
@@ -43,13 +44,38 @@ def parse_rearrangement(rearrangement):
   rearrangement = rearrangement.replace('from', '')
   rearrangement = rearrangement.replace('to', '')
   # print(rearrangement)
-  return rearrangement.split('  ')
+  return list(map(int, rearrangement.split('  ')))
+
+
+for i in range(0, len(crates)):
+  crates[i] = [x for x in crates[i] if x != '   ']
+  print(crates[i])
+
 
 for rearrangement in lines[10:]:
-  num_crates, stack_src, stack_des = parse_rearrangement(rearrangement)
+  num_crates, src_stack, des_stack = parse_rearrangement(rearrangement)
 
-  print(num_crates, stack_src, stack_des)
+  print(num_crates, src_stack, des_stack)
 
+  l = crates[src_stack - 1][len(crates[src_stack - 1]) - num_crates:]
+  l.reverse()
+  for i in l:
+    # print(i)
+    crates[des_stack - 1].append(i)
+
+  for i in range(0, num_crates):
+    if len(crates[src_stack - 1]) != 0:
+      crates[src_stack - 1].pop()
+    # print(crates[src_stack])
   
+  
+  # break;
+  for i in range(0, len(crates)):
+    print(str(i+1) + ' ', end='')
+    print(crates[i])
 
-  break;
+  print("\n>>>>>>>>>>>>>>>>>>>>>>\n")
+
+for i in range(0, len(crates)):
+  print(crates[i][len(crates[i]) - 1][1], end='')
+print()
